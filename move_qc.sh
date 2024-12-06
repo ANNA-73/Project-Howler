c7416eba1198c958435414a7843466a0fc047537
Anahid Amani <anahidamanimoghadam2024@u.northwestern.edu>
2024-12-05 20:15:23 -0600

    dlt

--- move_qc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /projects/b1042/HartmannLab/Howler
-mapfile -t my_list < QCbin.txt
-cd /projects/b1042/HartmannLab/Howler/bins
-for item in "${my_list[@]}"; do
-    cp ${item}.fa  ../QC
-    echo "Item: $item"
-done
